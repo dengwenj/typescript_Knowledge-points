bfc5bef0fc4721592be01e60bddb831e2e17e51c
dengwenjie <927561237@qq.com>
2021-11-23 22:24:07 +0800

    ts编译选项1

---
+++ 02/src/app.js
@@ -0,0 +1,5 @@
+console.log('app');
+var a;
+a = ['4', 'ff'];
+var b;
+b = { name: 'ss', age: 11 };
---
+++ 02/src/app.ts
@@ -0,0 +1,6 @@
+console.log('app');
+let a: string[]
+a = ['4', 'ff']
+
+let b:{name: string, [propName: string]: any}
+b = { name: 'ss', age: 11 }
