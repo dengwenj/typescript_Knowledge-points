233833c6ecffe15a0f1f36617b461b348f045c8a
dengwenjie <927561237@qq.com>
2021-11-27 16:20:09 +0800

    compilerOptions 编译器的选项

--- 02/src/app.js
+++
@@ -1,5 +0,0 @@
-console.log('app');
-var a;
-a = ['4', 'ff'];
-var b;
-b = { name: 'ss', age: 11 };
--- 02/src/app.ts
+++ 02/src/app.ts
@@ -1,6 +1,1 @@
-console.log('app');
-let a: string[]
-a = ['4', 'ff']
-
-let b:{name: string, [propName: string]: any}
-b = { name: 'ss', age: 11 }
+let a = 'dengwenjie'
