4359262f1647a5f7ec618f00b1aa99669cfb186f
dengwenjie <927561237@qq.com>
2021-11-30 22:28:29 +0800

    计分牌文件

---
+++ 项目练习/src/modules/Food.ts
@@ -0,0 +1,34 @@
+// 定义食物类
+export default class Food {
+    element: HTMLElement;
+    constructor() {
+        // 获取页面中的 food 元素并将其赋值给 element
+        this.element = document.querySelector('.food')!;
+    }
+
+    // 定义一个获取食物的 x 轴坐标的方法
+    get X() {
+        return this.element.offsetLeft;
+    }
+
+    // 定义一个获取食物的 y 轴坐标的方法
+    get Y() {
+        return this.element.offsetTop;
+    }
+
+    //定义一个修改食物的位置
+    changFood() {
+        // 生成一个随机的位置
+        // 食物的位置最小是 0 ，最大是 290
+        // 蛇移动一次就是一格，一格的大小就是 10，所以要求食物的坐标要整 10
+        // Math.round 四舍五入
+        let top = Math.round(Math.random() * 29) * 10;
+        this.element.style.left = top + 'px';
+        this.element.style.top = top + 'px';
+    }
+}
+
+const f = new Food();
+console.log(f.X, f.Y);
+f.changFood()
+console.log(f.X, f.Y);
---
+++ 项目练习/src/modules/ScorePanel.ts
@@ -0,0 +1,46 @@
+export default class ScorePanel {
+    // 分数
+    score = 0;
+    // 等级
+    level = 1;
+    scoreEle: HTMLElement;
+    levelEle: HTMLElement;
+    // 分数达到多个了就升一级
+    scoreUp: number;
+    // 等级上限
+    levelUp: number;
+
+    constructor(scoreUp: number = 10, levelUp: number = 10) {
+        this.scoreEle = document.querySelector('#score')!;
+        this.levelEle = document.querySelector('#level')!;
+
+        this.scoreUp = scoreUp;
+        this.levelUp = levelUp;
+    };
+
+    // 加分数
+    addScore() {
+        this.scoreEle.innerHTML = ++this.score + '';
+        if (this.score % this.scoreUp === 0) {
+            this.addLevel()
+        }
+    }
+
+    // 等级加
+    addLevel() {
+        if (this.level < this.levelUp) {
+            this.levelEle.innerHTML = ++this.level + '';
+        }
+    }
+}
+
+const s = new ScorePanel()
+s.addScore()
+s.addScore()
+s.addScore()
+s.addScore()
+s.addScore()
+s.addScore()
+s.addScore()
+s.addScore()
+s.addScore()
